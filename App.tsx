b838ae0792ec428b7a90f531485247a503dd94c0
Daniel <danigon1998@gmail.com>
2024-12-12 15:31:49 -0300

    Criado o Cronometro

--- App.tsx
+++ App.tsx
@@ -1,118 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {Component} from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity,TextInput, Button } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+interface IProps{
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+interface IState{
+  timer: number;
+  ultimo: number | undefined;
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+class App extends Component <IProps, IState>{
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  private timerInterval: NodeJS.Timeout | undefined = undefined;
+
+  constructor(props:IProps){
+    super(props);
+    this.state = {timer: 0.0, ultimo: undefined};
+  }
+
+  vai = () =>{
+    if(this.timerInterval){
+      clearInterval(this.timerInterval);
+      this.timerInterval = undefined;
+    }
+    else{
+      this.timerInterval = setInterval(() => {
+        this.setState((prevState) => ({
+          timer: prevState.timer + 0.1,
+        }));
+      }, 100);
+    }
+  }
+
+  limpar = () =>{
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+      this.timerInterval = undefined;
+    }
+    this.setState({ timer: 0.0, ultimo: this.state.timer});
+  }
+
+  render(): React.ReactNode {
+
+    return(
+      <View style={styles.container}>
+        <Image 
+          source={require('./src/cronometro.png')}
+          style={styles.cronometro}
+        />
+        <Text style={styles.timer}>{this.state.timer.toFixed(1)}</Text>
+
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn} onPress={this.vai}>
+            <Text style={styles.btnText}>
+              {this.timerInterval? 'Parar' : 'Vai'}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+            <Text style={styles.btnText}>Limpar</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+
+        <View style={styles.areaUltima}>
+          <Text style = {styles.ultima} >
+            Ultimo Tempo: {this.state.ultimo?.toFixed(1)}
+          </Text>
+        </View>
+
+      </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingTop:20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  cronometro: {
+    width: 250,
+    height: 250
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  timer: {
+    marginTop: -160,
+    fontSize: 65,
+    color: '#FFF',
+    fontWeight: 'bold'
   },
-  highlight: {
-    fontWeight: '700',
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
   },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areaUltima: {
+    marginTop: 40
+  },
+  ultima: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#FFF'
+  }
 });
 
 export default App;
