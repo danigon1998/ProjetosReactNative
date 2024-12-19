2b92d4c68277dc807485657e58a731ed119449f0
Daniel <danigon1998@gmail.com>
2024-12-19 16:30:57 -0300

    Atualizada novamente

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#121212"
+  },
+  text:{
+    color:"#fff",
+    fontSize:30,
+    fontWeight:"bold"
+  }
+})
