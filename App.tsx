bf9b2133b3878aaaf7a2b47a9747035afbc48a70
Daniel <danigon1998@gmail.com>
2024-12-21 22:39:19 -0300

    Animações feitas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {useRef, useEffect} from "react";
+import {View, Text, StyleSheet, Animated} from "react-native";
 
 export default function App(){
+
+  const larAnimada = useRef(new Animated.Value(0)).current;
+  const altAnimada = useRef(new Animated.Value(50)).current;
+  const opAnimada = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    
+    Animated.timing(larAnimada,{
+      toValue:100,
+      duration:1000,
+      useNativeDriver:false
+    }).start()
+
+  }, []);
+
+  let porcentagemLargura = larAnimada.interpolate({
+    inputRange:[0,100],
+    outputRange:["0%","100%"]
+  })
+
   return(
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Animated.View style ={{
+        width:porcentagemLargura,
+        height:altAnimada,
+        justifyContent:"center",
+        backgroundColor:"#4169E1",
+        opacity:opAnimada
+      }}>
+        <Text style={{
+          textAlign:"center",
+          color:"#fff",
+          fontSize:22,
+          fontWeight:"bold"
+        }}>Carregando...</Text>
+      </Animated.View>
     </View>
   )
 }
@@ -14,7 +47,6 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:"center",
     alignItems:"center",
-    backgroundColor:"#121212"
   },
   text:{
     color:"#fff",
